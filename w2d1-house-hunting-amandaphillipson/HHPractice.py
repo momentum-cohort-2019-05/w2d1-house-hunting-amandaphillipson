a19a14f938d08ca47008c9073a996268f1a598db
Amanda Phillipson <50961133+amandaphillipson@users.noreply.github.com>
2019-06-04 08:51:58 -0400

    Add files via upload
    Notes located in code on what I know isn't working, and what I am struggling to understand.

---
+++ w2d1-house-hunting-amandaphillipson/HHPractice.py
@@ -0,0 +1,33 @@
+## Calculate how many months to save downpayment for a home, based on:
+## annual salary, total cost, and current/monthly savings.
+## annual_salary = 120000
+## total_cost = 1000000
+
+
+# print("Enter your annual income:")
+
+# if input > 0 = annual_salary
+
+# print("Enter price of home:)
+
+# if input > 0 = total_cost
+
+
+# portion_down_payment = total_cost * 0.25
+# portion_saved = annual_salary * 0.10
+# current_savings = 0
+# month = 0
+# r = (portion_saved / 12) * (0.04 / 12)
+# current_savings = current_savings (current_savings * (r /12))
+
+# while current_savings < portion_down_payment:
+#     current_savings = current_savings + (current_savings * (r / 12)) + portion_saved
+    
+#     month = month + 1
+    
+#     print(month)
+
+
+## Finally able to run a basic loop!  However, the math is incorrect.  
+## Also, I do not yet understand how to allow a user to enter input that will run the code. Perhaps a boolean situation? 
+## More time needed to learn and problem-solve.
---
+++ w2d1-house-hunting-amandaphillipson/househunting.py
@@ -0,0 +1,28 @@
+# Calculate how many months to save downpayment for a home, based on:
+# annual salary, total cost, and current/monthly savings.
+
+# print("Enter your annual income:")
+# input = annual_salary
+# annual_salary = 120000
+# total_cost = 1000000
+
+annual_salary = 120000
+total_cost = 1000000
+portion_down_payment = total_cost * 0.25
+portion_saved = annual_salary * 0.10
+current_savings = 0
+month = 0
+r = (portion_saved / 12) * (0.04 / 12)
+current_savings = current_savings (current_savings * (r /12))
+
+while current_savings < portion_down_payment:
+    current_savings = current_savings + (current_savings * (r / 12)) + portion_saved
+    
+    month = month + 1
+    
+    print(month)
+
+
+# Finally able to run a basic loop!  However, the math is incorrect.  
+# Also, I do not yet understand how to allow a user to enter input that will run the code. Perhaps a boolean situation? 
+# More time needed to learn and problem-solve.
